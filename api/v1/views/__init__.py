22ca6590e6f3d7404d9095d78fe7422a73040d05
Matthew Zakari <zakarimatthew62@gmail.com>
2024-06-09 14:40:33 -0700

    RESTful API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+app_views = Blueprint('api_veiws', __name__, url_prefix='/api/v1')
 
 # Wildcard import is ignored by linters, don't worry about warning
-from . import index  # Assuming index.py in same folder
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,39 @@
-from flask import jsonify
-from . import app_views  # Assuming this file is in views folder
+#!/usr/bin/python3
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    """
-    Returns the API status as a JSON response.
-    ---
-    responses:
-        200:
-            description: The API is up and running.
-            content:
-                application/json:
-                    schema:
-                        type: object
-                        properties:
-                            status:
-                                type: string
-                                description: The API status.
-    """
-    return jsonify({'status': 'OK'})
+'''API Status'''
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/status', strict_slashes=False)
+def return_status():
+    '''Returns the API status.'''
+    return jsonify(status='OK')
+
+
+@app.route('/stats', strict_slashes=False)
+def get_stats():
+    '''Returns statistics about the number of stored objects.'''
+    stats = {
+        'states': storage.count(State),
+        'users': storage.count(User),
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+    }
+    return jsonify(stats)
+
+
+if __name__ == '__main__':
+    app.run()
 
